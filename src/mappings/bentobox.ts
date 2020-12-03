877daade5ff86a92e9e8957cce4bdfab0bfbba9b
Matthew Lilley <hello@matthewlilley.com>
2020-12-03 02:07:41 +0000

    Housekeeping
    - Add missing dev dependancies
    - Lint fix

--- src/mappings/bentobox.ts
+++ src/mappings/bentobox.ts
@@ -1,5 +1,5 @@
-import { BIG_INT_ONE, MEDIUM_RISK_LENDING_PAIR_MASTER } from "./helpers/constants";
-import { BentoBox, LendingPair, Token } from "../../generated/schema";
+import { BIG_INT_ONE, MEDIUM_RISK_LENDING_PAIR_MASTER } from './helpers/constants'
+import { BentoBox, LendingPair, Token } from '../../generated/schema'
 import {
   BentoBox as BentoBoxContract,
   LogDeploy,
@@ -8,8 +8,8 @@ import {
   LogSetMasterContractApproval,
   LogTransfer,
   LogWithdraw,
-} from "../../generated/BentoBox/BentoBox";
-import { BigInt, dataSource, log } from "@graphprotocol/graph-ts";
+} from '../../generated/BentoBox/BentoBox'
+import { BigInt, dataSource, log } from '@graphprotocol/graph-ts'
 
 import { LendingPair as LendingPairContract } from '../../generated/BentoBox/LendingPair'
 import { LendingPair as LendingPairTemplate } from '../../generated/templates'
@@ -18,33 +18,31 @@ import { getUser } from './helpers/getUser'
 import { getUserBentoTokenData } from './helpers/getUserBentoTokenData'
 
 export function handleLogDeploy(event: LogDeploy): void {
-
-  log.info("[BentoBox] Log Deploy {} {} {}", [
+  log.info('[BentoBox] Log Deploy {} {} {}', [
     event.params.clone_address.toHex(),
     event.params.data.toHex(),
     event.params.masterContract.toHex(),
-  ]);
-  
-  let bentoBox = BentoBox.load(dataSource.address().toHex());
+  ])
+
+  let bentoBox = BentoBox.load(dataSource.address().toHex())
 
   if (bentoBox == null) {
     const bentoBoxContract = BentoBoxContract.bind(dataSource.address())
-    bentoBox = new BentoBox(dataSource.address().toHex());
+    bentoBox = new BentoBox(dataSource.address().toHex())
     bentoBox.WETH = bentoBoxContract.WETH()
     bentoBox.lendingPairsCount = BigInt.fromI32(0)
   }
 
   bentoBox.lendingPairsCount = bentoBox.lendingPairsCount.plus(BIG_INT_ONE)
 
-  bentoBox.save();
+  bentoBox.save()
 
   // Already initilised
   if (LendingPair.load(event.params.clone_address.toHex())) {
-    return;
+    return
   }
-  
+
   if (event.params.masterContract == MEDIUM_RISK_LENDING_PAIR_MASTER) {
-    
     // Bind to contract for easy data access on creation
     const lendingPairContract = LendingPairContract.bind(event.params.clone_address)
 
@@ -90,99 +88,96 @@ export function handleLogDeploy(event: LogDeploy): void {
 
     LendingPairTemplate.create(event.params.clone_address)
   }
-
 }
 
 export function handleLogDeposit(event: LogDeposit): void {
-  log.info("[BentoBox] Log Deposit {} {} {} {} {}", [
+  log.info('[BentoBox] Log Deposit {} {} {} {} {}', [
     event.params.amount.toString(),
     event.params.from.toHex(),
     event.params.share.toString(),
     event.params.to.toHex(),
     event.params.token.toHex(),
-  ]);
+  ])
 
-  let bentoAddress = event.address.toHex();
-  
-  let token = Token.load(event.params.token.toHex());
+  const bentoAddress = event.address.toHex()
+
+  let token = Token.load(event.params.token.toHex())
 
   if (token == null) {
-    token = new Token(event.params.token.toHex());
-    token.bentoBox = bentoAddress;
-    token.totalShare = BigInt.fromI32(0);
-    token.totalAmount = BigInt.fromI32(0);
+    token = new Token(event.params.token.toHex())
+    token.bentoBox = bentoAddress
+    token.totalShare = BigInt.fromI32(0)
+    token.totalAmount = BigInt.fromI32(0)
   }
 
-  token.totalAmount = token.totalAmount.plus(event.params.amount);
-  token.totalShare = token.totalShare.plus(event.params.share);
-  token.save();
-  let user = getUser(event.params.to);
-  let userTokenData = getUserBentoTokenData(event.params.to, event.params.token);
-  userTokenData.share = userTokenData.share.plus(event.params.share);
-  userTokenData.save();
+  token.totalAmount = token.totalAmount.plus(event.params.amount)
+  token.totalShare = token.totalShare.plus(event.params.share)
+  token.save()
 
+  const user = getUser(event.params.to)
+  const userTokenData = getUserBentoTokenData(event.params.to, event.params.token)
+  userTokenData.share = userTokenData.share.plus(event.params.share)
+  userTokenData.save()
 }
 
 export function handleLogFlashLoan(event: LogFlashLoan): void {
-  log.info("[BentoBox] Log Flash Loan {} {} {} {}", [
+  log.info('[BentoBox] Log Flash Loan {} {} {} {}', [
     event.params.amount.toString(),
     event.params.feeAmount.toString(),
     event.params.token.toHex(),
     event.params.user.toHex(),
-  ]);
-  let token = Token.load(event.params.token.toHex());
-  token.totalAmount = token.totalAmount.plus(event.params.feeAmount);
-  token.save();
+  ])
+  const token = Token.load(event.params.token.toHex())
+  token.totalAmount = token.totalAmount.plus(event.params.feeAmount)
+  token.save()
 }
 
-export function handleLogSetMasterContractApproval(
-  event: LogSetMasterContractApproval
-): void {
-  log.info("[BentoBox] Log Set Master Contract Approval {} {} {}", [
+export function handleLogSetMasterContractApproval(event: LogSetMasterContractApproval): void {
+  log.info('[BentoBox] Log Set Master Contract Approval {} {} {}', [
     event.params.approved == true ? 'true' : 'false',
     event.params.masterContract.toHex(),
     event.params.user.toHex(),
-  ]);
-  let user = getUser(event.params.user);
-  let masterContractApproval = getMasterContractApproval(event.params.user, event.params.masterContract);
-  masterContractApproval.approved = event.params.approved;
-  masterContractApproval.save();
+  ])
+  const user = getUser(event.params.user)
+  const masterContractApproval = getMasterContractApproval(event.params.user, event.params.masterContract)
+  masterContractApproval.approved = event.params.approved
+  masterContractApproval.save()
 }
 
 export function handleLogTransfer(event: LogTransfer): void {
-  log.info("[BentoBox] Log Transfer {} {} {} {} {}", [
+  log.info('[BentoBox] Log Transfer {} {} {} {} {}', [
     event.params.amount.toString(),
     event.params.from.toHex(),
     event.params.share.toString(),
     event.params.to.toHex(),
     event.params.token.toHex(),
-  ]);
+  ])
 
-  let sender = getUserBentoTokenData(event.params.from, event.params.token);
-  sender.share = sender.share.minus(event.params.share);
-  sender.save();
+  const sender = getUserBentoTokenData(event.params.from, event.params.token)
+  sender.share = sender.share.minus(event.params.share)
+  sender.save()
 
-  let receiverUser = getUser(event.params.to);
-  let receiver = getUserBentoTokenData(event.params.to, event.params.token);
-  receiver.share = receiver.share.plus(event.params.share);
-  receiver.save();
+  const receiverUser = getUser(event.params.to)
+  const receiver = getUserBentoTokenData(event.params.to, event.params.token)
+  receiver.share = receiver.share.plus(event.params.share)
+  receiver.save()
 }
 
 export function handleLogWithdraw(event: LogWithdraw): void {
-  log.info("[BentoBox] Log Withdraw {} {} {} {} {}", [
+  log.info('[BentoBox] Log Withdraw {} {} {} {} {}', [
     event.params.amount.toString(),
     event.params.from.toHex(),
     event.params.share.toString(),
     event.params.to.toHex(),
     event.params.token.toHex(),
-  ]);
-  
-  let token = Token.load(event.params.token.toHex());
-  token.totalAmount = token.totalAmount.minus(event.params.amount);
-  token.totalShare = token.totalShare.minus(event.params.share);
-  token.save();
+  ])
 
-  let sender = getUserBentoTokenData(event.params.from, event.params.token);
-  sender.share = sender.share.minus(event.params.share);
-  sender.save();
+  const token = Token.load(event.params.token.toHex())
+  token.totalAmount = token.totalAmount.minus(event.params.amount)
+  token.totalShare = token.totalShare.minus(event.params.share)
+  token.save()
+
+  const sender = getUserBentoTokenData(event.params.from, event.params.token)
+  sender.share = sender.share.minus(event.params.share)
+  sender.save()
 }
--- src/mappings/pair.ts
+++ src/mappings/pair.ts
@@ -9,7 +9,7 @@ import {
   LogRemoveBorrow,
   LogRemoveCollateral,
   OwnershipTransferred,
-  Transfer
+  Transfer,
 } from '../../generated/templates/LendingPair/LendingPair'
 
 import { LendingPair } from '../../generated/schema'
@@ -21,7 +21,7 @@ export function handleApproval(event: Approval): void {
   log.info('[BentoBox:LendingPair] Approval {} {} {}', [
     event.params._owner.toHex(),
     event.params._spender.toHex(),
-    event.params._value.toString()
+    event.params._value.toString(),
   ])
 }
 
@@ -29,16 +29,16 @@ export function handleLogAddAsset(event: LogAddAsset): void {
   log.info('[BentoBox:LendingPair] Log Add Asset {} {} {}', [
     event.params.fraction.toString(),
     event.params.share.toString(),
-    event.params.user.toHex()
+    event.params.user.toHex(),
   ])
 
-  let lendingPair = LendingPair.load(event.address.toHex())
+  const lendingPair = LendingPair.load(event.address.toHex())
   lendingPair.totalSupply = lendingPair.totalSupply.plus(event.params.fraction)
   lendingPair.totalAssetShare = lendingPair.totalAssetShare.plus(event.params.share)
   lendingPair.save()
 
-  let user = getUser(event.params.user)
-  let userData = getUserLendingPairData(event.params.user, event.address)
+  const user = getUser(event.params.user)
+  const userData = getUserLendingPairData(event.params.user, event.address)
   userData.balanceOf = userData.balanceOf.plus(event.params.fraction)
   userData.save()
 }
@@ -47,9 +47,9 @@ export function handleLogAddBorrow(event: LogAddBorrow): void {
   log.info('[BentoBox:LendingPair] Log Add Borrow {} {} {}', [
     event.params.fraction.toString(),
     event.params.share.toString(),
-    event.params.user.toHex()
+    event.params.user.toHex(),
   ])
-  let lendingPair = LendingPair.load(event.address.toHex())
+  const lendingPair = LendingPair.load(event.address.toHex())
   lendingPair.totalBorrowFraction = lendingPair.totalBorrowFraction.plus(event.params.fraction)
   lendingPair.totalBorrowShare = lendingPair.totalBorrowShare.plus(event.params.share)
   lendingPair.save()
@@ -58,21 +58,21 @@ export function handleLogAddBorrow(event: LogAddBorrow): void {
 export function handleLogAddCollateral(event: LogAddCollateral): void {
   log.info('[BentoBox:LendingPair] Log Add Collateral {} {}', [
     event.params.share.toString(),
-    event.params.user.toHex()
+    event.params.user.toHex(),
   ])
-  let lendingPair = LendingPair.load(event.address.toHex())
+  const lendingPair = LendingPair.load(event.address.toHex())
   lendingPair.totalCollateralShare = lendingPair.totalCollateralShare.plus(event.params.share)
   lendingPair.save()
 
-  let user = getUser(event.params.user)
-  let userData = getUserLendingPairData(event.params.user, event.address)
+  const user = getUser(event.params.user)
+  const userData = getUserLendingPairData(event.params.user, event.address)
   userData.userCollateralShare = userData.userCollateralShare.plus(event.params.share)
   userData.save()
 }
 
 export function handleLogExchangeRate(event: LogExchangeRate): void {
   log.info('[BentoBox:LendingPair] Log Exchange Rate {}', [event.params.rate.toString()])
-  let lendingPair = LendingPair.load(event.address.toHex())
+  const lendingPair = LendingPair.load(event.address.toHex())
   lendingPair.exchangeRate = lendingPair.exchangeRate.plus(event.params.rate)
   lendingPair.save()
 }
@@ -81,14 +81,14 @@ export function handleLogRemoveAsset(event: LogRemoveAsset): void {
   log.info('[BentoBox:LendingPair] Log Remove Asset {} {} {}', [
     event.params.fraction.toString(),
     event.params.share.toString(),
-    event.params.user.toHex()
+    event.params.user.toHex(),
   ])
-  let lendingPair = LendingPair.load(event.address.toHex())
+  const lendingPair = LendingPair.load(event.address.toHex())
   lendingPair.totalSupply = lendingPair.totalSupply.minus(event.params.fraction)
   lendingPair.totalAssetShare = lendingPair.totalAssetShare.minus(event.params.share)
   lendingPair.save()
 
-  let user = getUserLendingPairData(event.params.user, event.address)
+  const user = getUserLendingPairData(event.params.user, event.address)
   user.balanceOf = user.balanceOf.minus(event.params.fraction)
   user.save()
 }
@@ -97,14 +97,14 @@ export function handleLogRemoveBorrow(event: LogRemoveBorrow): void {
   log.info('[BentoBox:LendingPair] Log Remove Borrow {} {} {}', [
     event.params.fraction.toString(),
     event.params.share.toString(),
-    event.params.user.toHex()
+    event.params.user.toHex(),
   ])
-  let lendingPair = LendingPair.load(event.address.toHex())
+  const lendingPair = LendingPair.load(event.address.toHex())
   lendingPair.totalBorrowFraction = lendingPair.totalBorrowFraction.minus(event.params.fraction)
   lendingPair.totalBorrowShare = lendingPair.totalBorrowShare.minus(event.params.share)
   lendingPair.save()
 
-  let user = getUserLendingPairData(event.params.user, event.address)
+  const user = getUserLendingPairData(event.params.user, event.address)
   user.userBorrowFraction = user.userBorrowFraction.minus(event.params.fraction)
   user.save()
 }
@@ -112,13 +112,13 @@ export function handleLogRemoveBorrow(event: LogRemoveBorrow): void {
 export function handleLogRemoveCollateral(event: LogRemoveCollateral): void {
   log.info('[BentoBox:LendingPair] Log Remove Collateral {} {}', [
     event.params.share.toString(),
-    event.params.user.toHex()
+    event.params.user.toHex(),
   ])
-  let lendingPair = LendingPair.load(event.address.toHex())
+  const lendingPair = LendingPair.load(event.address.toHex())
   lendingPair.totalCollateralShare = lendingPair.totalCollateralShare.minus(event.params.share)
   lendingPair.save()
 
-  let user = getUserLendingPairData(event.params.user, event.address)
+  const user = getUserLendingPairData(event.params.user, event.address)
   user.userCollateralShare = user.userCollateralShare.minus(event.params.share)
   user.save()
 }
@@ -126,9 +126,9 @@ export function handleLogRemoveCollateral(event: LogRemoveCollateral): void {
 export function handleOwnershipTransferred(event: OwnershipTransferred): void {
   log.info('[BentoBox:LendingPair] Ownership Transfered {} {}', [
     event.params.newOwner.toHex(),
-    event.params.previousOwner.toHex()
+    event.params.previousOwner.toHex(),
   ])
-  let lendingPair = LendingPair.load(event.address.toHex())
+  const lendingPair = LendingPair.load(event.address.toHex())
   lendingPair.owner = event.params.newOwner
   lendingPair.save()
 }
@@ -137,14 +137,14 @@ export function handleTransfer(event: Transfer): void {
   log.info('[BentoBox:LendingPair] Transfer {} {}', [
     event.params._from.toHex(),
     event.params._to.toHex(),
-    event.params._value.toString()
+    event.params._value.toString(),
   ])
-  let sender = getUserLendingPairData(event.params._from, event.address)
+  const sender = getUserLendingPairData(event.params._from, event.address)
   sender.balanceOf = sender.balanceOf.minus(event.params._value)
   sender.save()
 
-  let user = getUser(event.params._to)
-  let receiver = getUserLendingPairData(event.params._to, event.address)
+  const user = getUser(event.params._to)
+  const receiver = getUserLendingPairData(event.params._to, event.address)
   receiver.balanceOf = receiver.balanceOf.plus(event.params._value)
   receiver.save()
 }
