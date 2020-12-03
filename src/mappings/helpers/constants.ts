1a75574ea04d5a514561783ad1e3ed0f7520016e
Matthew Lilley <hello@matthewlilley.com>
2020-12-03 14:45:25 +0000

    Update src/mappings/helpers/constants.ts
    Co-authored-by: Keno <Clearwood@users.noreply.github.com>

--- src/mappings/helpers/constants.ts
+++ src/mappings/helpers/constants.ts
@@ -3,4 +3,4 @@ import { Address, BigInt } from '@graphprotocol/graph-ts'
 export const BIG_INT_ZERO = BigInt.fromI32(0)
 export const BIG_INT_ONE = BigInt.fromI32(0)
 export const MEDIUM_RISK_LENDING_PAIR_MASTER = Address.fromString('0xdae20fa3487e3fe47de1e7ea973fc42b6cfe4737')
-export const BETOBOX_ADDRESS = Address.fromString('0x42192Cc25e0feb6debbd5fB32180Ee706c80F322')
+export const BENTOBOX_ADDRESS = Address.fromString('0x42192Cc25e0feb6debbd5fB32180Ee706c80F322')
